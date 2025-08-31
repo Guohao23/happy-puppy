8573951aebd6df4238cd33773ee201678ea3065b
Guohao23 <154290666+Guohao23@users.noreply.github.com>
2025-08-31 23:44:11 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE = 'happy-puppy';
 const ASSETS = [
-  '/happy-puppy/',
-  '/happy-puppy/double.html',
+  '/',
+  '/double.html',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
@@ -28,6 +28,7 @@ self.addEventListener('activate', e => {
   );
 
 });
+
 
 
 
